3e5e4133f18fee138b94517e68809a57440fabb6
elntele <elntele@gmail.com>
2024-08-13 16:45:36 +0000

    add flavor and icon to wheterSarch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # list-your-product
 list and elaborate anything 
+# endereço do travis 
+https://app.travis-ci.com/elntele/list-your-product.svg?token=RL3TTWGGZNy1z4Ud3XNN&branch=main
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.scope.ProjectInfo.Companion.getBaseName
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -33,6 +35,27 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    //binding
+    viewBinding {
+        enable = true
+    }
+
+    productFlavors{
+        flavorDimensions += "product"
+
+        create("wetherSearch"){
+            dimension = "product"
+            applicationIdSuffix = ".wetherSearch"
+            manifestPlaceholders["appName"]= "WetherSearch"
+        }
+
+       create("goodMovieForTheLastNight") {
+            dimension ="product"
+            applicationIdSuffix =".goodMovieForTheLastNight"
+           manifestPlaceholders["appName"]= "goodMovieForTheLastNight"
+
+        }
+    }
 }
 
 dependencies {
@@ -45,4 +68,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    //koin
+    api("io.insert-koin:koin-android:4.0.0-RC1")
+    implementation(kotlin("script-runtime"))
 }
