85a9b3577f28d0c07c77ff5e1d8e74d59eae0403
elntele <elntele@gmail.com>
2024-08-18 13:45:03 +0000

    add folder for each applicatio and navigation file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ import com.android.build.gradle.internal.scope.ProjectInfo.Companion.getBaseName
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+   // id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,6 +28,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
@@ -49,10 +52,10 @@ android {
             manifestPlaceholders["appName"]= "WetherSearch"
         }
 
-       create("goodMovieForTheLastNight") {
+       create("movieForNight") {
             dimension ="product"
-            applicationIdSuffix =".goodMovieForTheLastNight"
-           manifestPlaceholders["appName"]= "goodMovieForTheLastNight"
+            applicationIdSuffix =".movieForNight"
+           manifestPlaceholders["appName"]= "movieForNight"
 
         }
     }
@@ -65,6 +68,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -72,7 +76,11 @@ dependencies {
     //navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
     //koin
     api("io.insert-koin:koin-android:4.0.0-RC1")
     implementation(kotlin("script-runtime"))
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/br/jc/list_your_product/MainActivity.kt
+++ app/src/main/java/com/br/jc/list_your_product/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.br.jc.list_your_product
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.br.jc.list_your_product.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        System.out.println("teste")
+        //here the activity_main, the xml of this Activity main class, is set in a variable as the
+        //main container to allocate the navigate. But for it work, in this layout need having a
+        // FragmentContainerView look that nav_host_fragment is the id of FragmentContainerView
+        // who is inside in activity_main.xml.
+        val navFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //here the above container is gaven to the attribute navController
+        navController = navFragment.navController
+        auth = Firebase.auth
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth == null) {
+            navController?.setGraph(R.navigation.feature_login)
+        } else {
+            navController?.setGraph(R.navigation.feature_login)
         }
+
     }
 }
